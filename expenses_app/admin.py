e12e9dd8c0a0ad6d1b948b2d8cd99a6a27388f8f
Szymon <szymonhab@gmail.com>
2015-10-31 20:48:38 +0100

    Add base admin

--- expenses_app/admin.py
+++ expenses_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Shop, BillRow, Bill
+
+admin.site.register(Category)
+admin.site.register(Shop)
+
+
+class BillRowInline(admin.TabularInline):
+    model = BillRow
+    extra = 3
+
+
+class BillAdmin(admin.ModelAdmin):
+    inlines = [BillRowInline]
+
+admin.site.register(Bill, BillAdmin)
